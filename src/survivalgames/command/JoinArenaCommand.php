aae36fd98e431453034f4898bef5e08e6a2a9eb7
MrDoni98 <danil.bartel@gmail.com>
2015-03-19 14:03:09 +0400

    переведено на русский

--- src/survivalgames/command/JoinArenaCommand.php
+++ src/survivalgames/command/JoinArenaCommand.php
@@ -7,7 +7,7 @@ use survivalgames\BaseCommand;
 class JoinArenaCommand extends BaseCommand {
 
 	public function __construct() {
-		parent::__construct("join", "Joins an arena.", "/sg join <arena-id>", ["j"]);
+		parent::__construct("join", "Joins an arena.", "/sg join <номер арены>", ["j"]);
 	}
 	
 	public function execute(CommandSender $sender, $alias, array $args) {
